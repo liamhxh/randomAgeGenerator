88f65320fa39184d96b0e27629591072c6a1a1cf
liam07he@gmail.com <HXH_Liam@liams-macbook-pro.local>
2018-01-12 13:03:45 -0800

    Initial Commit

---
+++ agingTests/agingTests.swift
@@ -0,0 +1,36 @@
+//
+//  agingTests.swift
+//  agingTests
+//
+//  Created by Liam He on 1/12/18.
+//  Copyright © 2018 Liam He. All rights reserved.
+//
+
+import XCTest
+@testable import aging
+
+class agingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
