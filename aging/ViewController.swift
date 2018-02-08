8cf5f006a9d1aa4c660c874b7a20e625d5774bd4
liam07he@gmail.com <HXH_Liam@Liams-MacBook-Pro.local>
2018-02-07 16:34:13 -0800

    this app random generate age of people from 5 to 91

--- aging/ViewController.swift
+++ aging/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource{
+                                        // ||||| by putting th UItableViewDataSource you do not need to make it an extention function
+
+    @IBOutlet weak var tableView: UITableView!
+
+    let people = ["Liam","Andy","Jeff","Ben","Emily","Chaoi"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
+        let randomAge = arc4random_uniform(91) + 5;
+
+        cell.textLabel?.text = people[indexPath.row]
+        cell.detailTextLabel?.text = "\(String(randomAge)) Years Old"
+
+        return cell
     }
 
 
+
 }
+
+//extension ViewController: UITableViewDataSource{
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return people.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
+//        let randomAge = arc4random_uniform(91) + 5;
+//
+//        cell.textLabel?.text = people[indexPath.row]
+//        cell.detailTextLabel?.text = "\(String(randomAge)) Years Old"
+//
+//        return cell
+//    }
+//}
 
